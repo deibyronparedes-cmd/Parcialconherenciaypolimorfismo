37b30abd5a62ef61399773dcb54325bbf8f70653
deiby paredes <deiby_ron.paredes@uao.edu.co>
2026-04-08 10:30:06 -0500

    Estudiante ahora hereda de Persona y se agrega sobrecarga de métodos

--- src/main/java/modelo/Estudiante.java
+++ src/main/java/modelo/Estudiante.java
@@ -1,62 +1,37 @@
 package modelo;
 
+public class Estudiante extends Persona {
 
-public class Estudiante {
-    private static final double PORC_DESARROLLO = 0.55;
-    private static final double PORC_MATEMATICA = 0.45;
-
-    private final int codigo;
-    private final String nombre;
     private double notaDesarrollo;
     private double notaMatematica;
 
     public Estudiante(int codigo, String nombre, double notaDesarrollo, double notaMatematica) {
-        this.codigo = codigo;
-        this.nombre = nombre;
+        super(codigo, nombre); // HERENCIA
         this.notaDesarrollo = notaDesarrollo;
         this.notaMatematica = notaMatematica;
     }
 
+    // POLIMORFISMO (puede ser sobrescrito)
     public double calcularDefinitiva() {
-        return (notaMatematica * PORC_MATEMATICA) + 
-                (notaDesarrollo * PORC_DESARROLLO);
+        return (notaDesarrollo * 0.55) + (notaMatematica * 0.45);
     }
 
     public String aprobar() {
-        if (calcularDefinitiva() >= 3.5) {
-            return "SI APRUEBA";
-        } else {
-            return "NO APRUEBA";
-        }
+        return calcularDefinitiva() >= 3.5 ? "SI APRUEBA" : "NO APRUEBA";
+    }
+
+    // SOBRECARGA 👇
+    public void modificarNota(double nuevaDesarrollo) {
+        this.notaDesarrollo = nuevaDesarrollo;
+    }
+
+    public void modificarNota(double nuevaDesarrollo, double nuevaMatematica) {
+        this.notaDesarrollo = nuevaDesarrollo;
+        this.notaMatematica = nuevaMatematica;
     }
 
     public void incrementarDesarrollo(double valor) {
-        if(valor>=0&&valor<=0.5){
-            if(notaDesarrollo+valor>5.0){
-                notaDesarrollo=5.0;
-            }else{
-                notaDesarrollo+=valor;
-            }
-        }
-    }    
-        
-                  
-    public void modificarNota(double nuevaDesarrollo, double nuevaMatematica) {
-        if (nuevaDesarrollo>=0&& nuevaDesarrollo<=5){
-            this.notaDesarrollo=nuevaDesarrollo;
-        }
-        
-        if (nuevaMatematica>=0&&nuevaMatematica<=5){
-            this.notaMatematica=nuevaMatematica;
-        }
-    }
-
-    public int getCodigo() {
-        return codigo;
-    }
-
-    public String getNombre() {
-        return nombre;
+        notaDesarrollo = Math.min(5.0, notaDesarrollo + valor);
     }
 
     public double getNotaDesarrollo() {
